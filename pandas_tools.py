83cfbb758ce41d556c1f2a419a0bf4243c7efa20
OFR-IIASA <fricko@iiasa.ac.at>
2017-09-20 20:36:00 +0200

    outline of some functions to be documented in this repository

---
+++ pandas_tools.py
@@ -0,0 +1,33 @@
+import pandas as pd
+
+# To write data to excel
+
+# To read data from a single excel sheet
+
+# To read data from multiple excel sheets
+
+# To sort index
+
+# To select data by columns equal to a single argument or multiple arguments
+
+# To select data by the index
+
+# To rename an index with a known structure
+
+# To rename an index with an unknown structure
+
+# To pivot data
+
+# To stack data
+
+# To create a dataframe from a dictionary
+
+# To create a dataframe from lists
+
+# Helper function for creating a filter 
+
+# Helper function to retrieve numerical columns
+
+# Helper function to retrieve non-numerical columns
+
+# 
